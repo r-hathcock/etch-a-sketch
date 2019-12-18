8d068f683eabf88df0702bed1eb5343d4b3eb9b7
Ryan Hathcock <rhath32@gmail.com>
2019-12-18 15:35:12 -0800

    Need to add event listener

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-for (let i = 0; i < 480; i++)
+
+
+function changeColor() {
+    alert("test");
+}
+
+function createCanvas() {
+    for (let i = 0; i < 480; i++)
     {
         var node = document.createElement("DIV");
         node.id = "div" + (i + 1);
         node.className = "node";
+        node.onmouseover = "changeColor()";
         document.getElementById("etch-container").appendChild(node);
     }
+}
+
+
+createCanvas();
+var x = document.getElementsByClassName("node");
+x.addEventListener("mouseover", changeColor());
+ 
